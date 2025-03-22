d7a80ee57c1fb74d06fc5b5c73d60c2943c8deca
codepnw <idchokms01@outlook.com>
2025-03-22 17:57:36 +0700

    database: update users schema

--- db/migrations/000002_add_users_table.up.sql
+++ db/migrations/000002_add_users_table.up.sql
@@ -1,12 +1,9 @@
 CREATE TABLE `users` (
     `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
     `password` VARCHAR(255) NOT NULL,
-    `is_admin` BOOLEAN NOT NULL DEFAULT FALSE
+    `is_admin` BOOLEAN NOT NULL DEFAULT FALSE,
+    `created_at` DATETIME DEFAULT NOW(),
+    `updated_at` DATETIME
 );
-
-ALTER TABLE `orders` 
-    ADD COLUMN `user_id` INT NOT NULL,
-    ADD CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`)
-    REFERENCES `users` (`id`);
